97195f0d018422c32b63214cba31a89b04dd4086
Jadoussy <vuotto.jade@gmail.com>
2025-12-12 08:35:06 +0100

    Ajout de l'index html et script.js

---
+++ script.js
@@ -0,0 +1,24 @@
+// ------------------------------------------
+//   2. Thème : Gestion du Panier d'Achat
+// ------------------------------------------
+// Mise en situation :
+// Un site d'e-commerce doit retenir la liste des articles (objets) du panier même s'il n'est pas connecté.
+
+// JSON et Manipulation de Tableaux
+
+//      Ex. 1 - Charger et Initialiser : Écrivez une fonction getPanier() qui charge la liste
+// d'articles depuis le localStorage. Si rien n'existe, elle doit retourner un tableau
+// vide ([]) pour éviter une erreur
+
+
+
+
+
+
+
+
+
+
+
+
+
